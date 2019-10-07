087e8fbbc5a54d1a075e1fec516e10d3a98920f7
codemouse <freedmouse@gmail.com>
2019-10-07 14:08:14 -0700

    got spotify to work, still in progress with omdb

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
-// TODO Grab the axios package...
-// @link https://www.npmjs.com/package/axios
-const axios = require("axios");
+// require our keys file
+var keys = require("./keys.js");
 
-// 1. LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+// make sure we have axios
+var axios = require("axios");
+
+// First, let's work on the spotify info
+var Spotify = require('node-spotify-api');
+
+// need credentials
+var spotify = new Spotify({
+  id: '20fb3a4b2d444377bc3025117cf7fc1d',
+  secret: 'ac337dafc1a249b7aa4da9b803f9ff39'
+});
+
+// pretty self explanatory. 
+let artistName = function (artist) {
+  return artist.name;
+}
+
+// pass the song name entered by user into the search function
+let spotifyThis = function (songName) {
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    // This all the information we want to display
+    var songs = data.tracks.items;
+
+    for (let i = 1; i < songs.length; i++) {
+      console.log(i);
+      console.log(`artist(s): ${songs[i].artists.map(artistName)}`);
+      console.log(`song name: ${songs[i].name}`);
+      console.log(`album: ${songs[i].album.name}`);
+      console.log(`song preview: ${songs[i].preview_url}`);
+      console.log("*******************************************************************************************");
+    }
+  });
+}
+
+// set up a switch statement to manage user choices
+var choice = function (caseData, functionData) {
+  switch (caseData) {
+    case 'spotify-this-song':
+      spotifyThis(functionData);
+      break;
+    case 'movie-this':
+      movieThis(functionData);
+      break;
+    default: console.log("LIRI Doesn't Recognize That");
+  }
+}
+
+// make it so, number 2
+var run = function (arg1, arg2) {
+  choice(arg1, arg2);
+}
+
+// capture user input. 
+run(process.argv[2], process.argv[3]);
 
 
-// 2. Make a new GitHub repository called liri-node-app and clone it to your computer.
 
 // 3. To retrieve the data that will power this app, you'll need to send requests using the `axios` package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
 
@@ -18,31 +73,49 @@ const axios = require("axios");
 //    * [Moment](https://www.npmjs.com/package/moment)
 
 //    * [DotEnv](https://www.npmjs.com/package/dotenv)
-
+const API_KEY = '1abe8770';
 // Run the axios.get function...
 // The axios.get function takes in a URL and returns a promise (just like $.ajax)
-axios.get("http://www.omdbapi.com").then(
-  function(response) {
-    // If the axios was successful...
-    // Then log the body from the site!
-    console.log(response.data);
-  },
+function movieThis() {
+  axios.get("http://www.omdbapi.com/?apikey=1abe8770&t=secondhand+lions").then(
+    function (response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log(response.data);
 
-  function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  }
-);
+  // for (let i = 1; i < movies.length; i++) {
+  //   console.log(i);
+  //   console.log(`Movie Name: ${movies[i].Title}`);
+  //   console.log(`Year Relesed: ${movies[i].Released}`);
+  //   console.log(`Rated: ${movies[i].Rating}`);
+  //   console.log(`Poster: ${movies[i].Poster}`);
+  //   console.log("*******************************************************************************************");
+  // }
+
+    },
+
+    function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    })
+
+  
+
+
+
+
+}
+movieThis();
--- keys.js
+++ keys.js
@@ -1,6 +1,13 @@
-console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsInTown = {
+
+}
+
+exports.omdb = {
+  key: process.env.API_KEY
+}
